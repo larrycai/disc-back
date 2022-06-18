3eef87787039cae9db1b322947e730cf9b99f8fd
Larry Cai <larry.caiyu@gmail.com>
2022-06-18 16:37:36 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,7 +1,14 @@
 from http.server import BaseHTTPRequestHandler
+import redis
 class handler(BaseHTTPRequestHandler):
   def do_GET(self):
     self.send_response(200)
     self.end_headers()
+    r = redis.Redis(
+    host= 'eu1-decent-amoeba-37317.upstash.io',
+    port= '37317',
+    password= '2bae71892aee4943ba60af8067ee190e', ssl=True)
+    r.set('foo','bar')
     self.wfile.write(self.headers.get('x-forwarded-for').encode())
-    return
+    self.wfile.write(r.get('foo'))
+
